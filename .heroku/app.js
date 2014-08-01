bb55fb0f3b062d96a3ee8a66f2fafbecaa8b9797
ChrisChares <chris@eunoia.cc>
2014-07-31 19:04:15 -0600

    serving from app folder instead of dist folder

--- .heroku/app.js
+++ .heroku/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/app'));
 
 app.listen(process.env.PORT || 3000);
